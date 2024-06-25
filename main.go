7f8de157c5a5c572dbfdf2da89ee3dced5b40850
saraswati <payalsinghps652050@gmail.com>
2024-06-25 16:07:37 +0530

    some changes

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	configg := config.NewConfig()
 	config.PooledConnectDB(configg)
 	r := gin.New()
+
 	tcp_port := configg.PORT
 	address := fmt.Sprintf(":%v", tcp_port)
 	r.Run(address) // listen and serve on 0.0.0.0:8000
