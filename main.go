76199dfad5ec130c5a9aed3961cf4f8cedd0d59b
saraswati <payalsinghps652050@gmail.com>
2024-06-25 16:09:24 +0530

    Configure PostgreSQL database connection and integrate Viper for configuration Add go.mod file for module dependency Added go.sum file for dependencies Added main.go file for application entry function created a config folder inside this Configure PostgreSQL database connection and integrate Viper for configuration

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"exoplanets_services/config"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	configg := config.NewConfig()
+	config.PooledConnectDB(configg)
+	r := gin.New()
+
+	tcp_port := configg.PORT
+	address := fmt.Sprintf(":%v", tcp_port)
+	r.Run(address) // listen and serve on 0.0.0.0:8000
+}
